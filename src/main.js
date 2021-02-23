8116753c9f54e098af4171198f6689cbaef5e8b9
newton <kamaunewton78@gmail.com>
2021-02-23 13:35:46 +0300

    import bootstrap vue and css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+import  BootstrapVue from "bootstrap-vue";
+import App from "./App.vue";
+
+//import bootstrap css 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+//initialize vue bootstrap
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
